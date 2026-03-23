75b21cd6f17370a400b06d4328a34c477c49058a
Pacolias <pacomolinac2003@gmail.com>
2026-03-23 10:11:51 +0100

    feat: added import for Dashboard and corresponding path to Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/login" element={<LoginPage />} />
-				<Route path="/register" element={<RegisterPage  />} />
-				<Route path ="/dashboard" element={"<div>Dashboard comming soon...</div>"} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path ="/dashboard" element={<DashboardPage />} />
 				<Route path ="/" element={<LoginPage />} />
 			</Routes>
 		</BrowserRouter>
