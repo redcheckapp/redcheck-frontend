98fc8d76256dae8fcc235548fdbf713d80c4e339
Pacolias <pacomolinac2003@gmail.com>
2026-03-23 10:07:23 +0100

    feat: SubjectResponse, TaskResponse and SubjectWithTasks interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,3 +12,26 @@ export interface RegisterForm {
     email: string;
     password: string;
 }
+
+export interface SubjectResponse {
+    id: number;
+    name: string;
+    description: string | null;
+    archived: boolean;
+}
+
+export interface TaskResponse {
+    id: number;
+    title: string;
+    description: string | null;
+    assignedDate: string;
+    deadline: string | null;
+    completedDate: string | null;
+    completed: boolean;
+    overdue: boolean;
+    subjectId: boolean;
+}
+
+export interface SubjectWithTasks extends SubjectResponse {
+    tasks: TaskResponse[];
+}
