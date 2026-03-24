27e03c75fa5530a7a125d848d9e8ce2e4fd90715
Pacolias <pacomolinac2003@gmail.com>
2026-03-24 16:14:24 +0100

    feat: postSubject, archiveSubject, deleteSubject and updateSubject

--- src/api/subjectApi.ts
+++ src/api/subjectApi.ts
@@ -1,7 +1,35 @@
 import api from "./axiosConfig";
-import type { SubjectResponse } from "../types";
+import type { SubjectRequest, SubjectResponse } from "../types";
 
 export const getSubjects = async (): Promise<SubjectResponse[]> => {
-    const response = await api.get("/subjects");
+    const response = await api.get(`subjects`);
+    return response.data;
+}
+
+export const postSubject = async (subjectData: SubjectRequest): Promise<SubjectResponse> => {
+    const response = await api.post(
+        `subjects`,
+        subjectData
+    );
+    return response.data;
+}
+
+export const archiveSubject = async (subjectId: number, archived: boolean): Promise<SubjectResponse> => {
+    const response = await api.patch(
+        `subjects/${subjectId}/archive`,
+        { archived }
+    );
+    return response.data;
+}
+
+export const deleteSubject = async (subjectId: number): Promise<void> => {
+    await api.delete(`subjects/${subjectId}`);
+}
+
+export const updateSubject = async (subjectId: number, subjectData: SubjectRequest): Promise<SubjectResponse> => {
+    const response = await api.put(
+        `subjects/${subjectId}`,
+        subjectData
+    );
     return response.data;
 }
