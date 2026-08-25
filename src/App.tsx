f268bc2ce28298139df3e2fa1bccc14a0c9c8824
Pacolias <pacomolinac2003@gmail.com>
2026-08-25 20:27:41 +0200

    feat: wrap application in LanguageProvider for multi-language support

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import TermsPage from './pages/TermsPage';
 import PrivacyPage from './pages/PrivacyPage';
 import DashboardPage from "./pages/DashboardPage";
 import { ThemeProvider } from './context/ThemeContext';
+import { LanguageProvider } from './context/LanguageContext';
 
 const App = () => {
     return (
-        <ThemeProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route path="/dashboard" element={<DashboardPage />} />
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/terms" element={<TermsPage />} />
-                    <Route path="/privacy" element={<PrivacyPage />} />
-                </Routes>
-            </BrowserRouter>
-        </ThemeProvider>
-    )
+        <LanguageProvider>
+            <ThemeProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/dashboard" element={<DashboardPage />} />
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/terms" element={<TermsPage />} />
+                        <Route path="/privacy" element={<PrivacyPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
+        </LanguageProvider>
+    );
 };
 
 export default App;
